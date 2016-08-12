06e9d9a24db7844755c4173f9da51f0afa709476
anonymous <anonymous>
2016-08-11 20:12:19 -0400

    Minor refactoring to spell check variable names

--- RomanNumeralConverter.c
+++ RomanNumeralConverter.c
@@ -10,17 +10,17 @@ int toNumeric(int * result, const char* roman) {
 }
 
 int validateRomanNumeral(const char* roman) {
-  int trackContigousOccrances=0;
+  int trackContiguousOccurrences=0;
   int i = 0;
   char candidate = roman[i];
   while (roman[i] != '\0') {
       if (roman[i] == candidate) {
-        trackContigousOccrances = trackContigousOccrances+1;
+        trackContiguousOccurrences = trackContiguousOccurrences+1;
       } else {
         candidate = roman[i];
         trackContigousOccrances = 1;
       }
-      int code = validate(trackContigousOccrances, roman[i]);
+      int code = validate(trackContiguousOccurrences, roman[i]);
       if (code == INVALID_ROMAN_ERROR_CODE) {
         return INVALID_ROMAN_ERROR_CODE;
       }
@@ -30,16 +30,15 @@ int validateRomanNumeral(const char* roman) {
 }
 
 int validate(int actualOccurances, char roman) {
-      int allowedContiguousOccurrances = getRuleFor(roman);
-      // printf("roman=%c, rule=%d, actualOccurances=%d\n", roman, allowedContiguousOccurrances, actualOccurances);
-      if (allowedContiguousOccurrances == SUCCESS_CODE) {
-        return SUCCESS_CODE;
-      }
-
-      if (actualOccurances > allowedContiguousOccurrances) {
-          return INVALID_ROMAN_ERROR_CODE;
-      }
+    int allowedContiguousOccurrences = getRuleFor(roman);
+    if (allowedContiguousOccurrences == SUCCESS_CODE) {
       return SUCCESS_CODE;
+    }
+
+    if (actualOccurances > allowedContiguousOccurrences) {
+        return INVALID_ROMAN_ERROR_CODE;
+    }
+    return SUCCESS_CODE;
 }
 
 int getRuleFor(char roman) {
