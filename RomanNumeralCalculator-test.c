14d5b3e5ec192ecccdf9f3b53091caa1d327fef1
anonymous <anonymous>
2016-08-12 22:27:26 -0400

    Added validation test cases, kata test cases and refactoring

--- RomanNumeralCalculator-test.c
+++ RomanNumeralCalculator-test.c
@@ -10,6 +10,12 @@ static void assertAddRomanNumerals(char *inputRoman1, char *inputRoman2, char *e
   ck_assert_str_eq(actual, expectedRoman);
 }
 
+static void assertAddRomanNumeralsInvalid(char *inputRoman1, char *inputRoman2, int expectedError){
+  char actual[16] = "";
+  int exitCode = addRomanNumerals(actual, inputRoman1, inputRoman2);
+  ck_assert_int_eq(exitCode, INVALID_ROMAN_ERROR_CODE);
+}
+
 START_TEST(addTests) {
   assertAddRomanNumerals("I","I","II");
   assertAddRomanNumerals("II","I","III");
@@ -19,6 +25,17 @@ START_TEST(addTests) {
   assertAddRomanNumerals("XIV","LX","LXXIV");
   assertAddRomanNumerals("II","II","IV");
   assertAddRomanNumerals("D","D","M");
+  assertAddRomanNumerals("MMX","XVI","MMXXVI");
+}
+END_TEST
+START_TEST(addValidationTests) {
+  assertAddRomanNumeralsInvalid("IIII","I",INVALID_ROMAN_ERROR_CODE);
+  assertAddRomanNumeralsInvalid("I","IIII",INVALID_ROMAN_ERROR_CODE);
+}
+END_TEST
+
+START_TEST(addValidationTests_sumExceeding3999){
+       assertAddRomanNumeralsInvalid("MMMCMXCVIII","II", INVALID_ROMAN_ERROR_CODE);
 }
 END_TEST
 
@@ -30,5 +47,7 @@ Suite* romanNumeralCalculatorSuite(void) {
 
     suite_add_tcase(romanCalculatorSuite, addTestCase);
     tcase_add_test(addTestCase, addTests);
+    tcase_add_test(addTestCase, addValidationTests);
+    tcase_add_test(addTestCase, addValidationTests_SumExceeding3999);
     return romanCalculatorSuite;
 }
--- RomanNumeralCalculator.c
+++ RomanNumeralCalculator.c
@@ -5,21 +5,34 @@
 #include "RomanNumeralCalculator.h"
 #include "RomanNumeralConverter.h"
 
+int isError(int exitCode);
+
 int addRomanNumerals(char* output, const char* roman1, const char* roman2){
   int exitCode;
   int num1;
   int num2;
 
   exitCode = toNumeric(&num1, roman1);
+  if (isError(exitCode)) {
+    return exitCode;
+  }
   exitCode = toNumeric(&num2, roman2);
-
+  if (isError(exitCode)) {
+    return exitCode;
+  }
   int sum = num1 + num2;
-  // printf("summation=%d\n", sum);
-
-  exitCode = toRoman(output, sum);
-  return exitCode;
+  if (sum > MAX_ROMAN_NUMERAL_VALUE) {
+    return INVALID_ROMAN_ERROR_CODE;
+  }
+  return toRoman(output, sum);
 }
 
+int isError(int code) {
+  if (code == INVALID_ROMAN_ERROR_CODE) {
+    return code;
+  }
+  return SUCCESS_CODE;
+}
 int subtractRomanNumerals(char* output, const char* roman1, const char* roman2){
   return 0;
 }
