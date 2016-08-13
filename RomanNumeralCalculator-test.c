58cde8f01552bf6022fcfdfcbd45486ebb396940
anonymous <anonymous>
2016-08-12 21:24:43 -0400

    Added test and code for addition of roman numerals

--- RomanNumeralCalculator-test.c
+++ RomanNumeralCalculator-test.c
@@ -1,9 +1,18 @@
 #include<stdio.h>
 #include <check.h>
 #include "RomanNumeralCalculator.h"
+#include "RomanNumeralConverter.h"
+
+static void assertAddRomanNumerals(char *inputRoman1, char *inputRoman2, char *expectedRoman){
+  char actual[16] = "";
+  int exitCode = addRomanNumerals(actual, inputRoman1, inputRoman2);
+  ck_assert_int_eq(exitCode, SUCCESS_CODE);
+  ck_assert_str_eq(actual, expectedRoman);
+}
 
 START_TEST(addTests) {
-  ck_assert_int_eq(1, 1);
+  assertAddRomanNumerals("I","I","II");
+  assertAddRomanNumerals("II","I","III");
 }
 END_TEST
 
--- RomanNumeralCalculator.c
+++ RomanNumeralCalculator.c
@@ -1,8 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 #include "RomanNumeralCalculator.h"
 #include "RomanNumeralConverter.h"
 
 int addRomanNumerals(char* output, const char* roman1, const char* roman2){
-  return 0;
+  int exitCode;
+  int num1;
+  int num2;
+
+  exitCode = toNumeric(&num1, roman1);
+  exitCode = toNumeric(&num2, roman2);
+
+  int sum = num1 + num2;
+  printf("summation=%d\n", sum);
+
+  exitCode = toRoman(output, sum);
+  return exitCode;
 }
 
 int subtractRomanNumerals(char* output, const char* roman1, const char* roman2){
