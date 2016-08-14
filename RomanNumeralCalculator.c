ea53d1eab52b57bec0d74f5141f4e568e9edb19e
anonymous <anonymous>
2016-08-13 21:51:37 -0400

    Refactored the code to remove duplication from addition and subtraction functions

--- RomanNumeralCalculator.c
+++ RomanNumeralCalculator.c
@@ -5,29 +5,24 @@
 #include "RomanNumeralCalculator.h"
 #include "RomanNumeralConverter.h"
 
+typedef enum {
+  Addition,
+  Subtraction
+} Operation;
+
+int computeRomanNumerals(char* output, Operation  operationToPerform, const char* roman1, const char* roman2);
+int performOperation(Operation operationToPerform, int num1, int num2);
 int isError(int exitCode);
 
 int addRomanNumerals(char* output, const char* roman1, const char* roman2){
-  int exitCode;
-  int num1;
-  int num2;
-
-  exitCode = toNumeric(&num1, roman1);
-  if (isError(exitCode)) {
-    return exitCode;
-  }
-  exitCode = toNumeric(&num2, roman2);
-  if (isError(exitCode)) {
-    return exitCode;
-  }
-  int sum = num1 + num2;
-  if (sum > MAX_ROMAN_NUMERAL_VALUE) {
-    return INVALID_ROMAN_ERROR_CODE;
-  }
-  return toRoman(output, sum);
+  return computeRomanNumerals(output, Addition, roman1, roman2);
 }
 
 int subtractRomanNumerals(char* output, const char* roman1, const char* roman2){
+  return computeRomanNumerals(output, Subtraction, roman1, roman2);
+}
+
+int computeRomanNumerals(char* output, Operation  operationToPerform, const char* roman1, const char* roman2){
   int exitCode;
   int num1;
   int num2;
@@ -40,11 +35,20 @@ int subtractRomanNumerals(char* output, const char* roman1, const char* roman2){
   if (isError(exitCode)) {
     return exitCode;
   }
-  int result = num1 - num2;
-  if (result > MAX_ROMAN_NUMERAL_VALUE || result <= 0) {
+  int computedValue = performOperation(operationToPerform, num1, num2);
+  if (computedValue > MAX_ROMAN_NUMERAL_VALUE || computedValue <= 0) {
     return INVALID_ROMAN_ERROR_CODE;
   }
-  return toRoman(output, result);
+  return toRoman(output, computedValue);
+}
+int performOperation(Operation operationToPerform, int num1, int num2) {
+  int computedValue;
+  if (operationToPerform == Addition) {
+    computedValue = num1 + num2;
+  } else if (operationToPerform == Subtraction) {
+    computedValue = num1 - num2;
+  }
+  return computedValue;
 }
 
 int isError(int code) {
